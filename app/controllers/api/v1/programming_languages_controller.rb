fce554a8b58c3731b81be8e95e38f0e6adc22801
Diego Lima <lima.j.diego@gmail.com>
2022-11-02 18:40:35 -0300

    Criado método criate com strong params

--- app/controllers/api/v1/programming_languages_controller.rb
+++ app/controllers/api/v1/programming_languages_controller.rb
@@ -4,6 +4,16 @@ module Api
             def index
                 render json: ProgrammingLanguage.all
             end
+
+            def create
+                ProgrammingLanguage.create(programming_languages_params)
+            end
+            
+            private
+            
+            def programming_languages_params
+                params.require(:programming_languages_params).permit(:name, :creator, :released)
+            end    
         end
     end
 end            
