a0aebe9774c9fda3f86d13e393d501396cec8adb
Diego Lima <lima.j.diego@gmail.com>
2022-11-02 18:39:11 -0300

    Add o método create na rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :programming_languages, only: %[index]
+      resources :programming_languages, only: %[index create]
     end
   end        
 end
