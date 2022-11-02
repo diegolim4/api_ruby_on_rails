b383196c7fb5c85477cf7034e59337e5d04be932
Diego Lima <lima.j.diego@gmail.com>
2022-11-02 19:07:12 -0300

    corrigido erro ao chamar a rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :programming_languages, only: %[index create]
+      resources :programming_languages, only: %i[index create]
     end
   end        
 end
